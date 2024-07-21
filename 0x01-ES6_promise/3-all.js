e6c156bff989ef2203c8182130d1781d9f7002ef
AnasJafir <dr.anas.jafir@gmail.com>
2024-07-21 22:55:31 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstname} ${fullName.lastname}`))
+    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
