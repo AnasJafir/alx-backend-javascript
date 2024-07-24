d65605b8d0b0db5271ed38716793fe3cd18ac472
AnasJafir <dr.anas.jafir@gmail.com>
2024-07-24 19:24:39 +0000

    complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-unresolved */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
