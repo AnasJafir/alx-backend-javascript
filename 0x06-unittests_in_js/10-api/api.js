2b9e1e67aa62da3d13f73d252f1d5ac9adba58df
AnasJafir <dr.anas.jafir@gmail.com>
2024-07-25 22:38:59 +0000

    Regex integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,33 +1,33 @@
 const express = require('express');
+
 const app = express();
 const port = 7865;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+  res.end('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
-    const id = req.params.id;
-    res.send(`Payment methods for cart ${id}`);
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
 });
 
 app.get('/available_payments', (req, res) => {
-    const payments = {
-        payment_methods: {
-            credit_cards: true,
-            paypal: false
-        }
-    };
-    res.json(payments);
+  const obj = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.json(obj);
 });
 
 app.post('/login', (req, res) => {
-    const { userName } = req.body;
-    res.send(`Welcome ${userName}`);
+  const username = req.body.userName;
+  res.end(`Welcome ${username}`);
 });
 
 app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
+  console.log('API available on localhost port 7865');
 });
