e1f39aa43b15be7114fc9772703778cbdf1db7ca
AnasJafir <dr.anas.jafir@gmail.com>
2024-07-25 21:53:44 +0000

    Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+  it('should return a 200 status code', (done) => {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct result', (done) => {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
