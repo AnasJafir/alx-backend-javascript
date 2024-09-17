29358ea34f7fe9a10ebe4ea5fa3fb8eaf3ec02ac
AnasJafir <dr.anas.jafir@gmail.com>
2024-09-17 23:44:54 +0100

    Handle console input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,17 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  if (!process.stdin.isTTY) {
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    const name = data.toString();
+    process.stdout.write(`Your name is: ${name}`);
     process.exit();
-  } else {
-    process.stdin.pause();
-  }
-});
-
-process.on('exit', () => {
-  if (!process.stdin.isTTY) {
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
-  }
-});
+  });
+}
