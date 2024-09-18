13f32ec149c2267e0b653097170e370e32346da2
AnasJafir <dr.anas.jafir@gmail.com>
2024-09-18 16:55:40 +0100

    Create small http server using http module of nodejs

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,10 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+const handleRequest = (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-});
-
-server.listen(1245, () => {
-    console.log('Server running at http://localhost:3000/');
-  });
+  };
+  const app = http.createServer(handleRequest);
+  app.listen(1245);
+  
+  module.exports = app;
