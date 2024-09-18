38d94572ea43d256f53aec3461b59b8085750be2
AnasJafir <dr.anas.jafir@gmail.com>
2024-09-18 17:46:44 +0100

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import router from './routes';
+
+const express = require('express');
+const app = express();
+
+app.use('/', router);
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
